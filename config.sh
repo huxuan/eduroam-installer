6d5e5e45fbb077515851acea0d9b58fe8e91cad4
huxuan <i@huxuan.org>
2016-04-20 01:28:08 +0800

    Remove duplicate config in config.sh.

--- config.sh
+++ config.sh
@@ -6,6 +6,3 @@ export CLIENT_SECRET="NbsROz14fz3wFjDe"
 export LOCAL_SCHOOL_REALM="pku.edu.cn"
 export PARENT_IP_ADDRESSES=("162.105.129.2" "162.105.129.5")
 export PARENT_SECRETS=()
-
-## DO NOT MODIFY THIS PART
-export DOLLAR='$'
